5a281b87d63713320afc42efc58fcad12ce7ff75
dairsaber <dairsaber@outlook.com>
2022-05-12 21:22:07 +0800

    更新分类

--- routes/api.go
+++ routes/api.go
@@ -66,6 +66,8 @@ func RegisterApiRoutes(r *gin.Engine) {
 		{
 			// 创建分类
 			cgcGroup.POST("", middlewares.AuthJWT(), cgc.Store)
+			// 更新分类
+			cgcGroup.PUT("/:id", middlewares.AuthJWT(), cgc.Update)
 		}
 
 	}
